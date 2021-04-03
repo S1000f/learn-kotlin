176d649fa4df5b33ec7057d21d6ccdda1321c30c
Nick <dohp.kim@gmail.com>
2021-04-03 22:51:41 +0900

    three-quotation string, nested functions

--- src/main/kotlin/chapter3/Chapter3.kt
+++ src/main/kotlin/chapter3/Chapter3.kt
@@ -61,6 +61,24 @@ fun chapter3() {
     val (number1, number2) = a infixPlus 4
     println(plussed)
     println("${number1} :: ${number2}")
+
+    // 확장된 String api
+    val exampleStr = "12.345-6.a"
+    // 3중따옴표: 3중따옴표 안에서는 \ 를 포함하여 어떠한 문자도 이스케이프 하지않고 정규식을 만들 수 있다
+    println(exampleStr.split("""\.|-""".toRegex()))
+    println(exampleStr.split(".", "-"))
+    println(exampleStr.split('.', '-'))
+
+    parsePath("/usr/local/apache-artemis/bin/artemis.sh")
+
+    val logo = """|  //
+                 .| //
+                 .|//\\
+    """.trimMargin(".")
+    println(logo)
+
+    val classpathInWindows = """C:\User\appdata\roaming\app"""
+    println(classpathInWindows)
 }
 
 // 구조분해 선언
@@ -126,3 +144,57 @@ var java.lang.StringBuilder.lastCharV3: Char
 // 가변인자 함수: 파라미터명 앞에 vararg 키워드를 붙인다
 fun sizeOf(vararg values: Int): Int = values.size
 fun sizeOfList(value: List<Int>): Int = value.size
+
+fun parsePath(path: String) {
+    val dir = path.substringBeforeLast("/")
+    val fullName = path.substringAfterLast("/")
+
+    val fileName = fullName.substringBeforeLast(".")
+    val extension = fullName.substringAfterLast(".")
+
+    println("directory: ${dir}, file name: ${fileName}, extension: ${extension}")
+}
+
+class Users(val id: Int, val name: String, val address: String)
+
+fun saveUser(users: Users) {
+    if (users.name.isEmpty()) {
+        throw IllegalArgumentException("Can't save user ${users.id}: empty Name")
+    }
+    if (users.address.isEmpty()) {
+        throw IllegalArgumentException("Can't save user ${users.id}: empty Address")
+    }
+}
+
+// 중첩 함수 사용
+fun saveUserV2(user: Users) {
+    fun validate(user: Users, value: String, fieldName: String) {
+        if (value.isEmpty()) {
+            throw IllegalArgumentException("Can't save user ${user.id}: empty ${fieldName}")
+        }
+    }
+    validate(user, user.name, "name")
+    validate(user, user.address, "address")
+}
+
+// 중첩함수는 자신이 속한 함수의 파라미터와 로컬변수에 직접 접근이 가능
+fun saveUserV3(user: Users) {
+    fun validate(value: String, fieldName: String) {
+        if (value.isEmpty()) {
+            throw IllegalArgumentException("Can't save user ${user.id}: empty ${fieldName}")
+        }
+    }
+    validate(user.name, "name")
+    validate(user.address, "address")
+}
+
+// 재사용되는 편의 메소드는 확장함수로 만들면 좋다. 이때 public 변수는 직접 접근이 가능하다
+fun Users.validateV4() {
+    fun validate(value: String, fieldName: String) {
+        if (value.isEmpty()) {
+            throw IllegalArgumentException("Can't save user ${id}: empty ${fieldName}")
+        }
+    }
+    validate(name, "name")
+    validate(address, "address")
+}

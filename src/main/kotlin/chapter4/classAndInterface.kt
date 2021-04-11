01fddcc7bf664fb0e8da0bcdcf79c48b4205e3c9
Nick <dohp.kim@gmail.com>
2021-04-11 16:55:00 +0900

    sealed class, construct, init, super

--- src/main/kotlin/chapter4/classAndInterface.kt
+++ src/main/kotlin/chapter4/classAndInterface.kt
@@ -1,6 +1,8 @@
 package chapter4
 
+import org.omg.CORBA.Context
 import java.io.Serializable
+import javax.print.attribute.AttributeSet
 
 fun chapter4() {
     val buttonClick = ButtonClick()
@@ -8,6 +10,14 @@ fun chapter4() {
     buttonClick.setFocus(true)
     buttonClick.showOff()
 
+    val milly = User4v4("milly")
+    println(milly.isSubscribed)
+
+    val sully = User4v4("Sully", false)
+    println(sully.isSubscribed)
+
+    val yeon = User4v4("Yeon", isSubscribed = true)
+    println(yeon.isSubscribed)
 }
 
 interface Clickable {
@@ -100,7 +110,7 @@ class Button : View {
 
     // inner 변경자가 없는 경우 기본적으로 바깥 클래스인 Button 을 참조하지 않는다
     // 따라서 바깥 클래스인 Button 이 Serializable 을 구현하지 않았더라도, 그 중첩클래스인 ButtonState 만 따로 직렬화가 가능하다
-    class ButtonState : State {}
+    class ButtonState : State
 
     // inner 변경자: 바깥 클래스인 Button 을 참조하는 중첩클래스를 만들어 준다
     inner class Inner {
@@ -108,4 +118,57 @@ class Button : View {
         // inner 변경자가 있더라도 바깥 클래스인 Button 에 접근하려면 this@Button 형식으로 접근해야 함
         fun getOuterReference(): String = this@Button.memberFiled
     }
+}
+
+sealed class ExprClass {
+    class Num(val value: Int) : ExprClass()
+    class Sum(val left: ExprClass, val right: ExprClass) : ExprClass()
+}
+
+fun evals(e: ExprClass): Int = when (e) {
+    is ExprClass.Num -> e.value
+    is ExprClass.Sum -> evals(e.left) + evals(e.right)
+}
+
+// (val nickname: String) -> 주생성자
+open class User4(val nickname: String)
+
+// construct: 생성자 키워드, init: 초기화 블럭
+class User4v2 constructor(nickname: String) {
+    val nickname: String
+
+    init {
+        this.nickname = nickname
+    }
+}
+
+// 초기화 블럭을 필드초기화와 합체, 어노테이션이나 가시성 변경자가 없다면 construct 키워드 생략가능
+class User4v3(nickname: String) {
+    val nickname = nickname
+}
+
+// 생성자 파라미터에도 디폴트값을 적용가능
+class User4v4(val nickname: String, val isSubscribed: Boolean = true)
+
+// 상속받은 부모 클래스에 생성자가 있는 경우 -> : 상속받은 클래스명(인자)
+class TwitterUser(nickname: String) : User4(nickname)
+
+// 아무런 인자가 없는 기본 생성자(default constructor)가 자동으로 만들어진다
+open class Nothing
+// 디폴트 생성자를 가진 클래스를 상속받을 때도 반드시 그 생성자를 호출해야만 한다
+class Anything : Nothing()
+
+// 이 클래스의 인스턴스화를 막기위해 하나뿐인 생성자에 private 변경자를 붙였다
+class UtilityClass private constructor()
+
+open class View4 {
+    constructor(ctx: Context) {}
+
+    constructor(ctx: Context, attr: String) {}
+}
+
+class MyButtons: View4 {
+    constructor(ctx: Context) : this(ctx, "HELLO") {}
+
+    constructor(ctx: Context, attr: String) : super(ctx, attr) {}
 }
